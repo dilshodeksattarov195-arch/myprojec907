30719dcff98c911477ff9c43bd57e037b268352a
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-30 14:07:07 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class searchEenderWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = searchEenderWorker()
+    print("Worker engine initialized.")
